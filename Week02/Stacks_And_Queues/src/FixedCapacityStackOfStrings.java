cb1d724aa6b4d8ecb00cc63879629a8e7b2c5dd1
Ronen Gilead-Raz <ronengi@gmail.com>
2017-06-05 16:32:53 +0300

    A String stack implementation using a fixed size array.

---
+++ Week02/Stacks_And_Queues/src/FixedCapacityStackOfStrings.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ronen Gilead-Raz <ronengi@gmail.com>
+ */
+public class FixedCapacityStackOfStrings {
+    
+    private String[] s;
+    private int N = 0;
+
+    public FixedCapacityStackOfStrings(int capacity) {
+        s = new String[capacity];
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public void push(String item) {
+        s[N++] = item;
+    }
+
+    public String pop() {
+        return s[--N];
+    }
+
+}
